c492ce5700b790b30fe8537cda9539bc3244d081
smartins010-jpg <smartins010@gmail.com>
2026-08-05 19:06:52 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'spartan-forge-v1-22-04-sem-gps';
+const CACHE = 'spartan-forge-v1-22-07-sem-localizacao-strava';
 const APP_SHELL = [
   './.nojekyll',
   './BUILD-V1.01.txt',
@@ -14,6 +14,8 @@ const APP_SHELL = [
   './BUILD-V1.22.01.txt',
   './BUILD-V1.22.03.txt',
   './BUILD-V1.22.04.txt',
+  './BUILD-V1.22.05.txt',
+  './BUILD-V1.22.07.txt',
   './audio/complete.mp3',
   './audio/go-impact.mp3',
   './audio/go.mp3',
